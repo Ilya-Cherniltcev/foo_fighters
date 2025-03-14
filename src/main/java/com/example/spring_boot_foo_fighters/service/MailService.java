3d33ce70589d9d2bc55eb9bc52dbb99afd36f9d6
Gunay Hashimova <gunayhashimova99@gmail.com>
2025-03-14 21:08:54 +0400

    add Locale

--- src/main/java/com/example/spring_boot_foo_fighters/service/MailService.java
+++ src/main/java/com/example/spring_boot_foo_fighters/service/MailService.java
@@ -2,6 +2,7 @@ package com.example.spring_boot_foo_fighters.service;
 
 import com.example.spring_boot_foo_fighters.client.MailSenderClient;
 import com.example.spring_boot_foo_fighters.client.dto.MailRequest;
+import com.example.spring_boot_foo_fighters.enumeration.MultiLang;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ public class MailService {
     private final MailSenderClient mailSenderClient;
 
     public void sendMail(MailRequest mailRequest) {
+        mailRequest.setText(MultiLang.TEXT.message());
         mailSenderClient.sendMail(mailRequest);
     }
 
--- src/main/java/com/example/spring_boot_foo_fighters/service/UserService.java
+++ src/main/java/com/example/spring_boot_foo_fighters/service/UserService.java
@@ -4,10 +4,11 @@ import com.example.spring_boot_foo_fighters.dto.UserDto;
 import com.example.spring_boot_foo_fighters.entity.UserEntity;
 import com.example.spring_boot_foo_fighters.exception.NotValidAgeException;
 import com.example.spring_boot_foo_fighters.mapper.UserMapper;
-import com.example.spring_boot_foo_fighters.rabbitmq.RabbitMqMessageSender;
+//import com.example.spring_boot_foo_fighters.rabbitmq.RabbitMqMessageSender;
 import com.example.spring_boot_foo_fighters.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,13 +16,13 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-    private final RabbitMqMessageSender rabbitMqMessageSender;
+//    private final RabbitMqMessageSender rabbitMqMessageSender;
 
     public UserEntity save(UserDto userDto) {
         if (userDto.getAge() < 6) {
             throw new NotValidAgeException("Your age less than 6...");
         }
-        rabbitMqMessageSender.send(userDto);
+//        rabbitMqMessageSender.send(userDto);
         return userRepository.save(userMapper.toUserEntity(userDto));
     }
 
